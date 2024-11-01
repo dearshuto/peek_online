67c5e25381643ad2da949d59be511d2db2a075bc
Shuto Shikama <@gmail.com>
2024-11-01 21:23:53 +0900

    フロントエンドの UI をライブラリ化

---
+++ peek_online_view/README.md
@@ -0,0 +1,7 @@
+lib
+コンポーネントライブラリ
+npm run build
+
+dev
+開発用 react アプリ
+npm run dev
---
+++ peek_online_view/dev/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PeekOnline from "peek_online_view";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <PeekOnline />
+  </React.StrictMode>,
+);
